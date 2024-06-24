84dc7b28a14fac06229bcfc132541d6d04c4c0b7
rayden_c@mail.ru <Anubis01>
2024-06-24 16:53:44 +0300

    some

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:superheroes/blocs/main_bloc.dart';
@@ -43,7 +45,7 @@ class MainPageContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final MainBloc bloc = Provider.of<MainBloc>(context);
+    final MainBloc bloc = Provider.of<MainBloc>(context, listen: false);
     return Stack(
       children: [
         const MainPageStateWidget(),
@@ -53,8 +55,69 @@ class MainPageContent extends StatelessWidget {
             text: "Next state",
             onTap: () => bloc.nextState(),
           ),
+        ),
+        const Padding(
+          padding: EdgeInsets.only(left: 16, right: 16, top: 12),
+          child: SearchWidget(),
         )
       ],
+    );
+  }
+}
+
+class SearchWidget extends StatefulWidget {
+  const SearchWidget({super.key});
+
+  @override
+  State<SearchWidget> createState() => _SearchWidgetState();
+}
+
+class _SearchWidgetState extends State<SearchWidget> {
+  final TextEditingController controller = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    SchedulerBinding.instance?.addPostFrameCallback((timestamp) {
+      final MainBloc bloc = Provider.of<MainBloc>(context, listen: false);
+      controller.addListener(() => bloc.updateText(controller.text));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      //onChanged: (text) => bloc.updateText(text),
+      style: const TextStyle(
+        fontWeight: FontWeight.w400,
+        fontSize: 20,
+        color: Colors.white,
+      ),
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: SuperheroesColors.indigo75,
+        isDense: true,
+        prefixIcon: const Icon(
+          Icons.search,
+          color: Colors.white54,
+          size: 24,
+        ),
+        suffix: GestureDetector(
+          onTap: () => controller.clear(),
+          child: Icon(
+            Icons.clear,
+            color: Colors.white,
+          ),
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: const BorderSide(color: Colors.white24)),
+      ),
     );
   }
 }
@@ -64,7 +127,7 @@ class MainPageStateWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final MainBloc bloc = Provider.of<MainBloc>(context);
+    final MainBloc bloc = Provider.of<MainBloc>(context, listen: false);
     return StreamBuilder<MainPageState>(
       stream: bloc.observeMainPageState(),
       builder: (context, snapshot) {
@@ -80,9 +143,9 @@ class MainPageStateWidget extends StatelessWidget {
           case MainPageState.noFavorites:
             return const NoFavoritesWidget();
           case MainPageState.favorites:
-            return const FavoritesWidget();
+            return SuperheroesList(title: "Your favorites", stream: bloc.observeFavoritesSuperheroes());
           case MainPageState.searchResults:
-            return const SearchResultWidget();
+            return SuperheroesList(title: "Search result", stream: bloc.observeSearchedSuperheroes());
           case MainPageState.nothingFound:
             return const NothingFoundWidget();
           case MainPageState.loadingError:
@@ -200,6 +263,63 @@ class FavoritesWidget extends StatelessWidget {
         ),
       ],
     );
+  }
+}
+
+/// SH LIST
+class SuperheroesList extends StatelessWidget {
+  final String title;
+  final Stream<List<SuperheroInfo>> stream;
+
+  const SuperheroesList({super.key, required this.title, required this.stream});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<List<SuperheroInfo>>(
+        stream: stream,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData || snapshot.data == null) {
+            return const SizedBox.shrink();
+          }
+          final List<SuperheroInfo> superheroes = snapshot.data!;
+
+          return ListView.separated(
+            itemCount: superheroes.length + 1,
+            itemBuilder: (BuildContext context, int index) {
+              if (index == 0) {
+                return Padding(
+                  padding: const EdgeInsets.only(left: 16, right: 16, top: 90, bottom: 20),
+                  child: Text(
+                    title,
+                    textAlign: TextAlign.left,
+                    style: GoogleFonts.openSans(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w800,
+                        color: Colors.white),
+                  ),
+                );
+              }
+              final SuperheroInfo item = superheroes[index - 1];
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: SuperheroCard(
+                  name: item.name,
+                  realName: item.realName,
+                  imageUrl: item.imageUrl,
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              SuperheroPage(name: item.name)),
+                    );
+                  },
+                ),
+              );
+            }, separatorBuilder: (BuildContext context, int index) {
+              return const SizedBox(height: 8,);
+          },
+          );
+        });
   }
 }
 
